d3dce676bc17b37a8447558d8b563487943d4f26
HackSlive <109677408+HackSlive@users.noreply.github.com>
2022-08-20 16:00:26 +0500

    Create авто продажа и авто фарм на ауре.lua

---
+++ авто продажа и авто фарм на ауре.lua
@@ -0,0 +1,11 @@
+while wait() do
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5028.55957, 39.7001724, 1598.66943, 0.385320723, 1.04119291e-08, 0.922782719, 2.57249333e-08, 1, -2.20249881e-08, -0.922782719, 3.22252092e-08, 0.385320723)
+
+wait(1.2)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(83.107048, 91246.5312, 120.392349, -0.95910418, -1.62063944e-08, 0.283053368, 3.03609626e-09, 1, 6.75431906e-08, -0.283053368, 6.56403287e-08, -0.95910418)
+
+wait(0.6)
+
+end
